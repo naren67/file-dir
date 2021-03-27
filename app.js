4bbacda6d7489f5b6d1b7fcd5fc7d7d700accb56
naren67 <75604512+naren67@users.noreply.github.com>
2021-03-27 08:17:39 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var serveIndex = require('serve-index')
-
-var app = express()
-
-app.use('/ftp',express.static('public/ftp'),serveIndex('public/ftp',{'icons':true}))
-
-app.listen(3000)
